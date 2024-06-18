67ab1ee609bbb53747d20b174585ae87ad73d3be
hongducdev <contact.hongduc@gmail.com>
2024-06-18 15:58:22 +0700

    Update border gradient for home page and navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
-import {BookHeart, Home, Settings2} from "lucide-react";
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger,} from "@/components/ui/tooltip";
-import {usePathname} from "next/navigation";
-import {cn} from "@/lib/utils";
+import { BookHeart, Home, Settings2 } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger, } from "@/components/ui/tooltip";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 interface INavbarItem {
@@ -38,28 +38,30 @@ const Navbar = () => {
 
 	return (
 		<nav
-			className="fixed bottom-10 left-1/2 -translate-x-1/2 bg-zinc-950/[0.8] border border-zinc-400 py-2 px-1 rounded-lg flex">
-			{NavbarList.map((item) => (
-				<TooltipProvider key={item.id}>
-					<Tooltip>
-						<TooltipTrigger asChild>
-							<Link
-								href={item.url}
-								className={cn(
-									"p-3 text-zinc-50 rounded-md hover:bg-olivia/[0.1] transition-colors duration-300 ease-in-out",
-									pathname === item.url &&
-									"bg-summer-dog text-zinc-950 hover:bg-olivia hover:text-zinc-950 mx-1"
-								)}
-							>
-								{item.icon}
-							</Link>
-						</TooltipTrigger>
-						<TooltipContent>
-							<p>{item.title}</p>
-						</TooltipContent>
-					</Tooltip>
-				</TooltipProvider>
-			))}
+			className="fixed bottom-10 left-1/2 -translate-x-1/2 bg-summer-dog rounded-lg p-0.5">
+			<div className="py-2 px-1 flex flex-row bg-zinc-950 rounded-lg">
+				{NavbarList.map((item) => (
+					<TooltipProvider key={item.id}>
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Link
+									href={item.url}
+									className={cn(
+										"p-3 text-zinc-50 rounded-md hover:bg-olivia/[0.1] transition-colors duration-300 ease-in-out",
+										pathname === item.url &&
+										"bg-summer-dog text-zinc-950 hover:bg-olivia hover:text-zinc-950 mx-1"
+									)}
+								>
+									{item.icon}
+								</Link>
+							</TooltipTrigger>
+							<TooltipContent>
+								<p>{item.title}</p>
+							</TooltipContent>
+						</Tooltip>
+					</TooltipProvider>
+				))}
+			</div>
 		</nav>
 	);
 };
