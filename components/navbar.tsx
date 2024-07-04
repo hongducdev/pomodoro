4ec731065de92322a46eaddb9bc916536f591b06
hongducdev <contact.hongduc@gmail.com>
2024-07-04 09:41:51 +0700

    refactor: Improve navbar component structure and styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,69 +1,89 @@
 "use client";
 import { BookHeart, Home, Settings2 } from "lucide-react";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger, } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import PlayLofi from "./play-lofi";
 
 interface INavbarItem {
-	id: number;
-	title: string;
-	url: string;
-	icon: React.ReactNode;
+  id: number;
+  title: string;
+  url?: string;
+  icon: React.ReactNode;
+  isButton?: boolean; // Add this property to differentiate between links and buttons
 }
 
 const NavbarList: INavbarItem[] = [
-	{
-		id: 1,
-		title: "Home",
-		url: "/",
-		icon: <Home/>,
-	},
-	{
-		id: 2,
-		title: "My Profile",
-		url: "/profile",
-		icon: <BookHeart/>,
-	},
-	{
-		id: 3,
-		title: "Settings",
-		url: "/settings",
-		icon: <Settings2/>,
-	},
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+    icon: <Home />,
+  },
+  {
+    id: 2,
+    title: "My Profile",
+    url: "/profile",
+    icon: <BookHeart />,
+  },
+  {
+    id: 3,
+    title: "Settings",
+    url: "/settings",
+    icon: <Settings2 />,
+  },
+  {
+    id: 4,
+    title: "Play Lofi",
+    icon: <PlayLofi />,
+    isButton: true,
+  },
 ];
 
 const Navbar = () => {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	return (
-		<nav
-			className="fixed bottom-10 left-1/2 -translate-x-1/2 bg-summer-dog rounded-lg p-0.5">
-			<div className="py-2 px-1 flex flex-row bg-zinc-950 rounded-lg">
-				{NavbarList.map((item) => (
-					<TooltipProvider key={item.id}>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Link
-									href={item.url}
-									className={cn(
-										"p-3 text-zinc-50 rounded-md hover:bg-olivia/[0.1] transition-colors duration-300 ease-in-out",
-										pathname === item.url &&
-										"bg-summer-dog text-zinc-950 hover:bg-olivia hover:text-zinc-950 mx-1"
-									)}
-								>
-									{item.icon}
-								</Link>
-							</TooltipTrigger>
-							<TooltipContent>
-								<p>{item.title}</p>
-							</TooltipContent>
-						</Tooltip>
-					</TooltipProvider>
-				))}
-			</div>
-		</nav>
-	);
+  return (
+    <div className="fixed bottom-10 left-1/2 -translate-x-1/2 flex items-center gap-10">
+      <nav className=" bg-summer-dog rounded-lg p-0.5">
+        <div className="py-2 px-1 flex flex-row bg-zinc-950 rounded-lg">
+          {NavbarList.map((item) => (
+            <TooltipProvider key={item.id}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  {item.isButton ? (
+                    <div className="p-3 text-zinc-50 rounded-md hover:bg-olivia/[0.1] transition-colors duration-300 ease-in-out mx-1">
+                      {item.icon}
+                    </div>
+                  ) : (
+                    <Link
+                      href={item.url || "/"}
+                      className={cn(
+                        "p-3 text-zinc-50 rounded-md hover:bg-olivia/[0.1] transition-colors duration-300 ease-in-out",
+                        pathname === item.url &&
+                          "bg-summer-dog text-zinc-950 hover:bg-olivia hover:text-zinc-950 mx-1"
+                      )}
+                    >
+                      {item.icon}
+                    </Link>
+                  )}
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{item.title}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ))}
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
--- components/play-lofi.tsx
+++ components/play-lofi.tsx
@@ -13,13 +13,8 @@ const PlayLofi = () => {
     }
   };
   return (
-    <div
-      className="group cursor-pointer fixed bottom-10 right-10 bg-summer-dog p-0.5 rounded-full"
-      onClick={handleClick}
-    >
-      <div className="w-14 h-14 rounded-full bg-zinc-900 text-zinc-50 flex items-center justify-center">
-        {playing ? <Pause className="w-6 h-6" /> : <Play className="w-6 h-6" />}
-      </div>
+    <div onClick={handleClick}>
+      {playing ? <Pause className="w-6 h-6" /> : <Play className="w-6 h-6" />}
     </div>
   );
 };
