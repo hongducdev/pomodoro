d2d8bf0cb65fe9fafeeca1c886199941c8559e08
hongducdev <contact.hongduc@gmail.com>
2024-07-04 09:06:52 +0700

    Update profile item component layout and styling

--- components/profile-item.tsx
+++ components/profile-item.tsx
@@ -9,30 +9,30 @@ const padTime = (time: number | string): string => {
 
 const ProfileItem = ({ profile }: { profile: IProfile }) => {
   return (
-    <Link href={`/profile/${profile.id}`}>
+    <div
+      className={`relative group w-[300px] h-[300px] rounded-md ${profile.background} p-0.5 transition-all duration-500 ease-in-out`}
+    >
       <div
-        className={`relative group w-[300px] h-[300px] rounded-md ${profile.background} p-0.5 transition-all duration-500 ease-in-out cursor-pointer`}
+        className={`flex h-full w-full items-center justify-center bg-zinc-900 rounded-md p-5 flex-col transition-all duration-300 ease-in-out group-hover:bg-opacity-0`}
       >
-        <div
-          className={`flex h-full w-full items-center justify-center bg-zinc-900 rounded-md p-5 flex-col transition-all duration-300 ease-in-out group-hover:bg-opacity-0`}
-        >
-          <span className="text-3xl absolute top-5 left-5 flex items-center transition-all duration-300 ease-in-out">
-            {profile.icon}
-          </span>
-          <EditProfile profile={profile} />
+        <span className="text-3xl absolute top-5 left-5 flex items-center transition-all duration-300 ease-in-out">
+          {profile.icon}
+        </span>
+        <EditProfile profile={profile} />
+        <Link href={`/profile/${profile.id}`}>
           <h5
-            className={`text-5xl font-semibold text-zinc-50 transition-all duration-300 ease-in-out group-hover:text-zinc-800`}
+            className={`text-5xl font-semibold text-zinc-50 transition-all duration-300 ease-in-out group-hover:text-zinc-800 cursor-pointer hover:underline`}
           >
             {profile.name}
           </h5>
-          <span
-            className={`absolute font-medium text-zinc-50 transition-all duration-300 ease-in-out bottom-5 left-1/2 -translate-x-1/2 group-hover:text-zinc-800`}
-          >
-            {padTime(profile.minutes)} : {padTime(profile.seconds)}
-          </span>
-        </div>
+        </Link>
+        <span
+          className={`absolute font-medium text-zinc-50 transition-all duration-300 ease-in-out bottom-5 left-1/2 -translate-x-1/2 group-hover:text-zinc-800`}
+        >
+          {padTime(profile.minutes)} : {padTime(profile.seconds)}
+        </span>
       </div>
-    </Link>
+    </div>
   );
 };
 
